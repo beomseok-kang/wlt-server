fd8c8569147939c287499d417219e766e3c52624
bumsuk980119 <beom.kang17@gmail.com>
2020-10-06 08:17:48 +0100

    socket changed

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const hpp = require('hpp');
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const hpp = require("hpp");
 
 dotenv.config();
 
-const indexRouter = require('./routes');
-const webSocket = require('./services/socket');
-const connectToMongo = require('./schemas');
-const logger = require('./logger');
+const indexRouter = require("./routes");
+const webSocket = require("./services/socket");
+const connectToMongo = require("./schemas");
+const logger = require("./logger");
 
 const app = express();
 
+app.set("port", process.env.NODE_ENV === "proeduction" ? 80 : 8000);
 // connect to MongoDB
-connectToMongo();
+// connectToMongo();
 
 // middlewares
-if (process.env.NODE_ENV === 'production') {
-    app.use(morgan('combined'));
-    app.use(helmet());
-    app.use(hpp());
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(helmet());
+  app.use(hpp());
 } else {
-    app.use(morgan('dev'));
+  app.use(morgan("dev"));
 }
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 const sessionOptions = {
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false
-    }
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 };
 // if (process.env.NODE_ENV === 'production') {
 //     sessionOptions.proxy = true;
@@ -48,21 +49,21 @@ const sessionMiddleware = session(sessionOptions);
 app.use(sessionMiddleware);
 
 // router
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // favicon and other 404
-app.get('/favicon.ico', (req, res) => res.status(204));
+app.get("/favicon.ico", (req, res) => res.status(204));
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
 // error handler
 app.use((err, req, res, next) => {
-    logger.error(err);
+  logger.error(err);
 });
 
 // server (listener)
-
-module.exports = app;
+const server = app.listen(app.get("port"));
+webSocket(server, app);
--- services/socket.js
+++ services/socket.js
@@ -1,43 +1,36 @@
-const SocketIO = require('socket.io');
-const axios = require('axios');
-const logger = require('../logger');
+const SocketIO = require("socket.io");
+const logger = require("../logger");
 
 module.exports = (server, app) => {
-    const io = SocketIO(server, { path: '/socket' });
-    app.set('io', io);
-    const room = io.of('/room');
+  const io = SocketIO(server, { path: "/socket" });
+  app.set("io", io);
+  const room = io.of("/room");
 
-    io.use((socket, next) => {
-        sessionMiddleware(socket.request, socket.request.res, next);
+  room.on("connection", (socket) => {
+    const req = socket.request;
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    const teamName = req._query.team;
+
+    // join namespace room of teamName
+    socket.join(teamName);
+    console.log(teamName);
+
+    // socket disconnection
+    socket.on("disconnect", () => {
+      socket.leave(teamName);
     });
 
-    room.on('connection', (socket) => {
-        const req = socket.request;
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        const teamName = req._query.team;
-
-        // join namespace room of teamName
-        socket.join(teamName);
-        console.log('hello user! of: ', teamName);
-
-        // socket disconnection
-        socket.on('disconnect', () => {
-            console.log('Client Exit: ', ip, socket.id);
-            socket.leave(teamName);
-        });
-
-        // chatting emit to room (to sockets in the same room)
-        socket.on('chat', (data) => {
-            logger.info(`new chat: ${teamName}, ${data.chat} ___ ${ip}`, );
-            socket.to(teamName).emit('new-chat', data);
-        });
-
-        // socket error
-        socket.on('error', (err) => {
-            logger.error(err);
-        });
-
-        // socket
-
+    // chatting emit to room (to sockets in the same room)
+    socket.on("chat", (data) => {
+      logger.info(`new chat: ${teamName}, ${data.chat} ___ ${ip}`);
+      socket.to(teamName).emit("new-chat", data);
     });
+
+    // socket error
+    socket.on("error", (err) => {
+      logger.error(err);
+    });
+
+    // socket
+  });
 };
